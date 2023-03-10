6e811a4cfc9765221e3408a4f9d80476a52a4db4
Peter Buchegger <peterus@users.noreply.github.com>
2023-03-10 20:32:16 +0000

    fix admin

--- web/admin.py
+++ web/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 
-from .models import Organization, Node, Board
+from .models import Organization, UserMembership, Device, Board, BoardStatus, Run
 
 admin.site.register(Organization)
-admin.site.register(Node)
+admin.site.register(UserMembership)
+admin.site.register(Device)
 admin.site.register(Board)
+admin.site.register(BoardStatus)
+admin.site.register(Run)
